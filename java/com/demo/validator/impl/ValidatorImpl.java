c720ca8817c3f6507c3756e9746e1c570f7cb10c
Anand Rajneesh <anandrajneesh@outlook.com>
2013-09-30 13:32:14 +0200

    Formatted

--- java/com/demo/validator/impl/ValidatorImpl.java
+++ java/com/demo/validator/impl/ValidatorImpl.java
@@ -13,32 +13,33 @@ import com.demo.validator.models.Car;
 public class ValidatorImpl {
 
 	private static Validator validator;
-	
-	
+
 	public static void main(String[] args) {
 		try {
-			
-			ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+
+			ValidatorFactory validatorFactory = Validation
+					.buildDefaultValidatorFactory();
 			validator = validatorFactory.getValidator();
-			
+
 			Car c = new Car();
 			c.setManufacturer("ho@");
-			
-			Set<ConstraintViolation<Car>>  validationIssues = validator.validate(c);
-			
-			for(Iterator<ConstraintViolation<Car>> iter = validationIssues.iterator(); iter.hasNext();){
+
+			Set<ConstraintViolation<Car>> validationIssues = validator
+					.validate(c);
+
+			for (Iterator<ConstraintViolation<Car>> iter = validationIssues
+					.iterator(); iter.hasNext();) {
 				ConstraintViolation<Car> voilation = iter.next();
 				print(voilation.getMessage());
 				print(voilation.getPropertyPath().toString());
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void print(String s){
+
+	public static void print(String s) {
 		System.out.println(s);
 	}
 }
